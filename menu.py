786572127e49b65dd44fb197b949728459ed33cf
Jan <jancarlagarcia@gmail.com>
2026-01-29 10:27:59 -0300

    Update menu.py
    Fazendo todo laço do Menu Funcionar antes de simplifica-lo em funções

--- menu.py
+++ menu.py
@@ -1,3 +1,13 @@
+from time import sleep
+import os
+
+def limpar_tela():
+    os.system('cls')
+
+def pausar():
+    input('Pressione ENTER para voltar ao MENU PRINCIPAL!')
+
+
 #Título
 print('-' * 60)
 print(f"{'BOOKCATALOG':^60}")
@@ -28,21 +38,61 @@ while True:
 
     if opcao == 1:
         print('Cadastrar Livro (em breve).')
+        pausar()
     
     elif opcao == 2:
         print('Listar Livros Não Lidos (em breve)')
+        pausar()
     
     elif opcao == 3:
         print('Listar Livros Finalizados (em breve)')
+        pausar()
     
     elif opcao == 4:
         print('Listar Biblioteca Completa (em breve)')
+        pausar()
     
     elif opcao == 5:
         print('Marcar Livro como Finalizado (em breve)')
+        pausar()
     
     elif opcao == 6:
-        print('Apagar Livro')
+        continuar_excluindo = True
+
+        while continuar_excluindo:
+            excluir_livro = input('Digite o Título que Deseja Excluir: ')
+
+            while True:
+                resp = input(f'Tem Certeza que Deseja Excluir {excluir_livro}? [S/N] ').upper().strip()
+
+                if resp == 'S':
+                    print('LIVRO EXCLUÍDO COM SUCESSO!')
+
+                    while True:
+                        op = input('Deseja Excluir Outro Exemplar? [S/N] ').upper().strip()
+
+                        if op == 'S':
+                            break
+
+                        elif op == 'N':
+                            print('Voltando ao MENU PRINCIPAL...')
+                            sleep(0.6)
+                            continuar_excluindo = False
+                            break
+
+                        else:
+                            print('Opção Inválida! Digite S ou N.')
+                            
+                    break
+
+                elif resp == 'N':
+                    print('Exclusão Encerrada.')
+                    sleep(0.6)
+                    continuar_excluindo = False
+                    break
+
+                else: 
+                    print('Opção inválida! Digite S ou N.')
     
     elif opcao == 7:
         while True:
@@ -53,7 +103,7 @@ while True:
                 exit()
 
             elif resp =='N':
-                print('Voltando para o Menu...')
+                print('Voltando ao MENU PRINCIPAL...')
                 break
 
             else:
